2b43c09f92be251940d6d102c12bb75cc707c20d
Jonny He <jonnybravo_he@hotmail.com>
2023-03-07 00:01:44 +0100

    added a simple add function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-console.log('Hello World');
-alert("This is my personal website, for now.");
+//console.log('Hello World');
+//alert("This is my personal website, for now.");
 
+const zero = 0;
+const one = 1;
+
+function add(int1, int2) {
+    result = int1 + int2;
+    console.log(result);
+}
+
+add(zero, one);
+add(one, one);
 
 
