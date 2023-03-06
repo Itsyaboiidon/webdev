4ba04fd220c2659d90825dd3f6ee7d28e94171cf
Jonny He <jonnybravo_he@hotmail.com>
2023-03-07 00:27:28 +0100

    made a new multiply function

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ function add(int1, int2) {
     console.log(result);
 }
 
+function mult(x, y) {
+    result = x * y;
+    console.log(result);
+}
+
 add(zero, one);
 add(one, one);
-
-
+add(1, 2);
+mult(zero, one);
+mult(1, 2)
